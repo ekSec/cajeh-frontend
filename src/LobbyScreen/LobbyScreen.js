7e3bfa7b7c245f0f2e5ab021cae619442eb750c8
Caje <daniel_dancho@hotmail.com>
2019-10-14 10:19:43 -0300

    cajehOriginOcultAndOk

--- src/LobbyScreen/LobbyScreen.js
+++ src/LobbyScreen/LobbyScreen.js
@@ -75,7 +75,7 @@ export default class Lobby extends Component {
                   })
                 }
               >
-                obbyhi
+                obby
               </Text>
             </Title>
           </Body>
